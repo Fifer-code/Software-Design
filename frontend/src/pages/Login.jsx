eb6d39fcd0a62d82315bb288a66be0e5fb390c97
timmydhoang <timmy.hoang314460@gmail.com>
2026-02-02 16:55:21 -0600

    Initialized React App w/ placeholder page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+
+function Login() {
+    return (
+        <div style={{ textAlign: "center", marginTop: "4rem" }}>
+            <h1>QueueSmart Login Test Page (Placeholder)</h1>
+
+            <input type="text" placeholder="Username" style={{ display: "block", margin: "1rem auto" }}/>
+
+            <input type="password" placeholder="Password" style={{ display: "block", margin: "1rem auto" }}/>
+        
+            <button>Login</button>
+        </div>
+
+    );
+
+}
+
+export default Login;
