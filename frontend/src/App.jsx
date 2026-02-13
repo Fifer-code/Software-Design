87d67e40632bc2d67116dac9a38491fe01377ffc
timmydhoang <timmy.hoang314460@gmail.com>
2026-02-12 19:33:00 -0600

    added registration page and css styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import UserDashboard from "./pages/UserDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         {/* If url is = path, render the specified element (ie. page/component). Need to add validation later. */}
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/user/dashboard" element={<UserDashboard />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
