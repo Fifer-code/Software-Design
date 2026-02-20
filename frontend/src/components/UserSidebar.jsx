4e3dad0776fe9b325837231467aee9b17eb4069f
timmydhoang <timmy.hoang314460@gmail.com>
2026-02-20 14:58:32 -0600

    Added UserSidebar.css and altered styling

--- frontend/src/components/UserSidebar.jsx
+++ frontend/src/components/UserSidebar.jsx
@@ -1,24 +1,26 @@
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-import { Link, Outlet } from "react-router-dom";
-import "/src/userdashboard.css";
+import { NavLink, Outlet } from "react-router-dom";
+import "./UserSidebar.css";
 
 function UserSidebar() {
   return (
     <div className="sidebar-layout">
       <Sidebar>
         <div className="sidebar-main">
-          <Menu>
-            <MenuItem component={<Link to="/user/dashboard" />}>Dashboard</MenuItem>
-            <MenuItem component={<Link to="/user/join" />}>Join Queue</MenuItem>
-            <MenuItem component={<Link to="/user/status" />}>Queue Status</MenuItem>
-            <MenuItem component={<Link to="/user/history" />}>History</MenuItem>
-            <MenuItem component={<Link to="/user/feedback" />}>Feedback</MenuItem>
-          </Menu>
+          <div className="sidebar-menu-container">
+            <Menu>
+              <MenuItem component={<NavLink to="/user/dashboard" />}>Dashboard</MenuItem>
+              <MenuItem component={<NavLink to="/user/join" />}>Join Queue</MenuItem>
+              <MenuItem component={<NavLink to="/user/status" />}>Queue Status</MenuItem>
+              <MenuItem component={<NavLink to="/user/history" />}>History</MenuItem>
+              <MenuItem component={<NavLink to="/user/feedback" />}>Feedback</MenuItem>
+            </Menu>
+          </div>
 
           <div className="sidebar-footer">
             <div className="sidebar-avatar" />
             <div className="sidebar-user">User</div>
-            <Link to="/" className="sidebar-logout">Logout</Link>
+            <NavLink to="/" className="sidebar-logout">Logout</NavLink>
           </div>
         </div>
       </Sidebar>
