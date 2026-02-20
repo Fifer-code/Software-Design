4858445c75a61483e8d4120a9e10f71be3ac2a30
Gustavo Arriaga <arriagagustavo42@gmail.com>
2026-02-20 16:09:50 -0600

    Finished all admin screens

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -14,16 +14,16 @@ function AdminDashboard() {
             <div className = "admin-card-1">
                 <h1>List of Services</h1>
                 <div className = "admin-subcard">
-                    <p>DMV</p>
+                    <h3>DMV</h3>
                 </div>
                 <div className = "admin-subcard">
-                    <p>Banking</p>
+                    <h3>Banking</h3>
                 </div>
                 <div className = "admin-subcard">
-                    <p>Student Advising</p>
+                    <h3>Student Advising</h3>
                 </div>
                 <div className = "admin-subcard">
-                    <p>placeholder</p>
+                    <h3>placeholder</h3>
                 </div>
             </div>
             <div className = "admin-card-2">
@@ -32,21 +32,25 @@ function AdminDashboard() {
                     <h3>DMV Queue 1</h3>
                     <p>People in Queue: 18</p>
                     <p>Estimated Wait: 45 minutes</p>
+                    <p>Priority: Low</p>
                 </div>
                 <div className = "admin-subcard">
                     <h3>Banking Queue 1</h3>
                     <p>People in Queue: 7</p>
                     <p>Estimated Wait: 25 minutes</p>
+                    <p>Priority: Medium</p>
                 </div>
                 <div className = "admin-subcard">
                     <h3>Student Advising Queue 1</h3>
                     <p>People in Queue: 4</p>
                     <p>Estimated Wait: 95 minutes</p>
+                    <p>Priority: High</p>
                 </div>
                 <div className = "admin-subcard">
                     <h3>placeholder</h3>
                     <p>People in Queue: </p>
                     <p>Estimated Wait: </p>
+                    <p>Priority:</p>
                 </div>
             </div>
             <div className = "admin-card-3">
--- frontend/src/pages/admin/QueueManagement.jsx
+++ frontend/src/pages/admin/QueueManagement.jsx
@@ -10,25 +10,198 @@ function QueueManagement(){
             <h2>Queue Management</h2>
             <div className = "admin-card-container">
                 <div className = "admin-card-1">
-                    <h1>Avaialabe Queues</h1>
-                    <p>Queue 1</p>
-                    <p>Queue 2</p>
-                    <p>Queue 3</p>
-                    <p>Queue 4</p>
-                    <p>Queue 5</p>
-                    <p>Queue 6</p>
-                </div>
-                <div className = "admin-card-2">
-                    <h1>Queue Information</h1>
-                    <div className = "admin-subcard">
-                        <h1>Reorder/Remove Users</h1>
-                        <p>user 1</p>
-                        <p>user 2</p>
-                    </div>
-                    <div className = "admin-subcard">
-                        <h1>Serve Next User</h1>
-                        <p>user 1</p>
-                        <p>user 2</p>
+                    <h1>Avaialabe Queues & Information</h1>
+
+                    <div className = "queue-grid-container">
+                        <div className = "admin-subcard">
+                            <h3>DMV Queue 1</h3>
+                            <div className = "queue-description">
+                                <p>People in Queue: 18</p>
+                                <p>Estimated Wait: 45 minutes</p>
+                                <p>Priority: Low</p>
+                                <button type= "submit">Serve Next User</button>
+                            </div>
+                            <div className = "queue-list-container">
+                            <div className = "queue-user">
+                                    <p>User 1</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 2</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 3</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 4</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 5</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 6</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 7</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className = "admin-subcard">
+                            <h3>Banking Queue 1</h3>
+                            <div className = "queue-description">
+                                <p>People in Queue: 7</p>
+                                <p>Estimated Wait: 25 minutes</p>
+                                <p>Priority: Medium</p>
+                                <button type= "submit">Serve Next User</button>
+                            </div>
+                            <div className = "queue-list-container">
+                                <div className = "queue-user">
+                                    <p>User 1</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 2</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 3</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 4</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 5</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 6</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 7</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className = "admin-subcard">
+                            <h3>Student Advising Queue 1</h3>
+                            <div className = "queue-description">
+                                <p>People in Queue: 4</p>
+                                <p>Estimated Wait: 95 minutes</p>
+                                <p>Priority: High</p>
+                                <button type= "submit">Serve Next User</button>
+                            </div>
+                            <div className = "queue-list-container">
+                                <div className = "queue-user">
+                                    <p>User 1</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 2</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 3</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                                <div className = "queue-user">
+                                    <p>User 4</p>
+                                    <div className = "queue-user-actions">
+                                        <button className = "queue-move-buttons">Move up</button>
+                                        <button className = "queue-move-buttons">Move Down</button>
+                                        <button className = "queue-move-buttons">Remove</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className = "admin-subcard">
+                            <h3>placeholder</h3>
+                            <div className = "queue-description">
+                                <p>People in Queue: </p>
+                                <p>Estimated Wait: </p>
+                                <p>Priority:</p>
+                                <button type= "submit">Serve Next User</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
