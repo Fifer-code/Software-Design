f33b83bee66f46c8894692caf80016f0e8e8a47b
timmydhoang <timmy.hoang314460@gmail.com>
2026-02-20 18:01:33 -0600

    Changed AdminSidebar to match UserSidebar and use same styling

--- frontend/src/components/AdminSidebar.jsx
+++ frontend/src/components/AdminSidebar.jsx
@@ -1,27 +1,38 @@
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-import { Link} from "react-router-dom";
-import "./AdminSidebar.css";
+import { NavLink, Outlet } from "react-router-dom";
+import "./UserSidebar.css";
 
 function AdminSidebar() {
   return (
-    <>
-      <Sidebar width="100%" className="admin-sidebar">
+    <div className="sidebar-layout">
+      <Sidebar>
         <div className="sidebar-main">
-          <Menu>
-            <MenuItem component={<Link to="/admin/dashboard" />}>Dashboard</MenuItem>
-            <MenuItem component={<Link to="/admin/queuemanagement" />}>Queue <br></br> Management</MenuItem>
-            <MenuItem component={<Link to="/admin/servicemanagement" />}>Service <br></br> Management</MenuItem>
-          </Menu>
+          <div className="sidebar-header">
+            <div className="sidebar-logo">A</div>
+            <h2 className="sidebar-title">QueueSmart</h2>
+          </div>
+
+          <div className="sidebar-menu-container">
+            <Menu>
+              <MenuItem component={<NavLink to="/admin/dashboard" />}>Dashboard</MenuItem>
+              <MenuItem component={<NavLink to="/admin/queuemanagement" />}>Queue Management</MenuItem>
+              <MenuItem component={<NavLink to="/admin/servicemanagement" />}>Service Management</MenuItem>
+            </Menu>
+          </div>
 
           <div className="sidebar-footer">
             <div className="sidebar-avatar" />
             <div className="sidebar-user">Admin</div>
-            <Link to="/" className="sidebar-logout">Logout</Link>
+            <NavLink to="/" className="sidebar-logout">Logout</NavLink>
           </div>
         </div>
       </Sidebar>
-    </>
-    );
+
+      <main style={{ flex: 1, padding: "20px" }}>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default AdminSidebar;
